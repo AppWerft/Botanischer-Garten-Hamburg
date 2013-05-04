e1c2bcc9e6cc6d5011ef38d03ad40e0ee12d58ac
AppWerft <rs@hamburger-appwerft.de>
2013-05-04 23:25:02 +0200

    UHH Login

--- Resources/module/uhh.window.js
+++ Resources/module/uhh.window.js
@@ -1,13 +1,21 @@
 exports.create = function() {
 	var self = require('module/win').create('UHH✦intern');
 	self.backgroundImage = 'Default.png';
+	var active = false;
 	var dialogwindow = require('uhhlogin/dialog.window').create();
 	self.addEventListener('focus', function() {
 		dialogwindow.open();
+		console.log('FOCUS');
+		active = true;
+		setTimeout(function() {
+			active = false
+		}, 100);
 	});
 	self.addEventListener('blur', function() {
-		//dialogwindow.close();
+		console.log('BLUR');
+		if (!active)
+			dialogwindow.close();
 	});
-	self.add(self.tv);
+
 	return self;
 }
