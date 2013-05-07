2e6e338b097ad37e123f403aed9661e0fa56cab1
AppWerft <rs@hamburger-appwerft.de>
2013-05-07 16:00:28 +0200

    Template recursive

--- Resources/module/TEMPLATES.js
+++ Resources/module/TEMPLATES.js
@@ -5,33 +5,39 @@ exports.plantrow = {
 		backgroundColor : 'white'
 	},
 	childTemplates : [{
-		type : 'Ti.UI.Label',
-		bindId : 'deutsch',
-		properties : {
-			color : '#060',
-			height : 30,
-			font : {
-				fontSize : 20,
-				fontWeight : 'bold'
+		type : 'Ti.UI.View',
+		height : Ti.UI.FILL,
+		width : Ti.UI.FILL,
+		layout : 'vertical',
+		childTemplates : [{
+			type : 'Ti.UI.Label',
+			bindId : 'deutsch',
+			properties : {
+				color : '#060',
+				height : 30,
+				font : {
+					fontSize : 20,
+					fontWeight : 'bold'
+				},
+				left : 10,
+				top : 5,
+				width : Ti.UI.FILL,
 			},
-			left : 10,
-			top : 5,
-			width : Ti.UI.FILL,
-		},
-		events : {}
-	}, {
-		type : 'Ti.UI.Label',
-		bindId : 'latein',
-		properties : {
-			font : {
-				fontFamily : 'FreeSerifItalic',
-				fontStyle : 'italic'
+			events : {}
+		}, {
+			type : 'Ti.UI.Label',
+			bindId : 'latein',
+			properties : {
+				font : {
+					fontFamily : 'FreeSerifItalic',
+					fontStyle : 'italic'
+				},
+				left : 10,
+				top : 35,
+				width : Ti.UI.FILL,
+				height : Ti.UI.SIZE
 			},
-			left : 10,
-			top : 35,
-			width : Ti.UI.FILL,
-			height : Ti.UI.SIZE
-		},
-		events : {}
+			events : {}
+		}]
 	}]
 };
