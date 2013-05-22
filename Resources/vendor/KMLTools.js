24452b6bde67c048c3570771f05ca3f7ab69fd11
AppWerft <rs@hamburger-appwerft.de>
2013-05-22 18:20:54 +0200

    using of native xmp parser => parse code modified because the json code has other node names.

--- Resources/vendor/KMLTools.js
+++ Resources/vendor/KMLTools.js
@@ -1,24 +1,24 @@
 function _parseKML(_kmlstring) {
-	var XMLTools = require("vendor/XMLTools");
-	var kml = new XMLTools(_kmlstring);
-	var polygones = kml.toObject().Document.Folder.Placemark;
+	var xml2json = require('ti.xml2json');
+	var json = xml2json.convert(_kmlstring);
+	var polygones = json.kml.Document.Folder.Placemark;
 	var vertices = {}, regions = {};
 	for (var i = 0; i < polygones.length; i++) {
-		var coords = polygones[i].Polygon.outerBoundaryIs.LinearRing.coordinates.split(' ');
-		vertices[polygones[i].name] = [];
-		regions[polygones[i].name] = {};
+		var coords = polygones[i].Polygon.outerBoundaryIs.LinearRing.coordinates.text.split(' ');
+		vertices[polygones[i].name.text] = [];
+		regions[polygones[i].name.text] = {};
 		var sumlat = 0, sumlon = 0;
 		for (var c = 0; c < coords.length - 1; c++) {
-			vertices[polygones[i].name].push({
+			vertices[polygones[i].name.text].push({
 				latitude : parseFloat(coords[c].split(',')[1]),
 				longitude : parseFloat(coords[c].split(',')[0])
 			});
 			sumlat += parseFloat(coords[c].split(',')[1]);
 			sumlon += parseFloat(coords[c].split(',')[0]);
 		}
-		regions[polygones[i].name].latitude = sumlat / (coords.length - 1);
-		regions[polygones[i].name].longitude = sumlon / (coords.length - 1);
-	}
+		regions[polygones[i].name.text].latitude = sumlat / (coords.length - 1);
+		regions[polygones[i].name.text].longitude = sumlon / (coords.length - 1);
+	}console.log(vertices);
 	return {
 		areas : vertices,
 		centers_of_areas : regions
