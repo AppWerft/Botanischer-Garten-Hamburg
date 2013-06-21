8f63813cb1c64b659b55c3af0cffee214485f431
AppWerft <rs@hamburger-appwerft.de>
2013-06-21 17:02:45 +0200

    Polygoneditor persistiert

--- webeditor/app.js
+++ webeditor/app.js
@@ -15,25 +15,43 @@
 	 $('#map').height(h);	
 	 $('#map').width($(window).width());
 	 var polygones = {};	
-	
-	function createPolygone(_polygone){		  
+	var key;
+	function createPolygone(_polygone,name){		  
 		var points = [];
     	for (var i=0;i<_polygone.length;i++) {
     	  		points.push(new google.maps.LatLng(_polygone[i][0],_polygone[i][1]));
     	}
     	var poly = new google.maps.Polygon({map : map,
-                                        strokeColor   : '#ff0000',
-                                        strokeOpacity : 0.6,
-                                        strokeWeight  : 3,
-                                        path:points,
-                                        map:map,run:false
+        	strokeColor   : '#ff0000',
+            strokeOpacity : 0.6,
+            strokeWeight  : 3,
+            path:points,   
+            map:map,
+            title : name,
+            run:false
         });
         google.maps.event.addListener(poly, 'click', function(_e){
         	if (poly.run ) {
         		poly.run=false;
         		poly.stopEdit();
+        		if (!key) key  = window.prompt("Um die Fläche zu speichern brauchts den geheimen Schlüssel.");
+        		var vertex = poly.getPath().getArray();
+        		var points = [];
+        		for (var i=0;i<vertex.length;i++) {
+        			points.push([vertex[i].lat(),vertex[i].lng()]);
+        		}
+        		$.ajax({
+        			success: function(){
+        			},
+        			url : 'api/',
+        			dataType : 'json',
+        			type : 'POST',
+        			data : 'key='+ encodeURI(key) + '&name='+ poly.title + '&vertex='+ encodeURI(JSON.stringify(points))
+        		});
+        		$('#v_'+ poly.title.replace(/[^a-z]/g,'')).detach();
         	} else {;
 	        	poly.runEdit();
+	        	$('#display').append('<p id="v_'+poly.title.replace(/[^a-z]/g,'')+'">'+poly.title+'</p>');
 	        	poly.run=true;
 	        }
         });
@@ -42,22 +60,8 @@
 	}
 	$.getJSON('api/',function(_polygones){
 	for (var name  in _polygones) {
-		  polygones[name] = createPolygone(_polygones[name]);
+		  polygones[name] = createPolygone(_polygones[name],name);
     }});
-    $('#start').live('click',function(){ 
-        if (run) {
-        	for (var name in polygones) {
-        		polygones[name].stopEdit(true);
-    			run = false;
-    		}	
-    	} else {	
-		    for (var name in polygones) {
-        		polygones[name].runEdit(true);
-    			run = false;
-    		}	
-   			run = true;
-   			
-    	}
-    });
+   
     
   }
