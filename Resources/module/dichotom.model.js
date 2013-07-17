1d22de9dccbbe92d6a81314fbd6654e0f589567e
AppWerft <rs@hamburger-appwerft.de>
2013-07-17 18:06:46 +0200

    alle types of intkey are realized

--- Resources/module/dichotom.model.js
+++ Resources/module/dichotom.model.js
@@ -3,28 +3,49 @@ var Dichotom = function(_name) {
 	this.tree = JSON.parse(f.read().text);
 	var key = this.tree.metadata.page_context_id.split('_');
 	key.pop();
-	this.id = key.join('_') ; 
+	this.treeId = key.join('_') + '_decisiontree';
 	return this;
 }
-Dichotom.prototype.getId = function() {
-	return this.id
-};
-Dichotom.prototype.getDecisionByCode = function(_args) {
-	var code = _args.code || 1;
-	var id = _args.id + '_';
+
+Dichotom.prototype.getDecisionById = function(_id) {
+	if (!_id) {
+		console.log('id initial setting to start-treeId "' + this.treeId + '"')
+		_id = this.treeId;
+	}
+	var regex = /_decisiontree/i;
+	if (_id.match(regex)) {// new tre
+		console.log('Id was new treeId "' + _id + '"')
+		this.treeId = _id + '_';
+		// persist
+		_id = undefined;
+		// id is invalid, we take the first decision of tree
+	} else {
+		console.log('we leave in tree: ' + this.treeId);
+	}
+	// Findig  tree:
 	for (var i = 0; i < this.tree.content.length; i++) {
-		if (this.tree.content[i].type == 'decisiontree' &&  this.tree.content[i].id== id) {
+		if (this.tree.content[i].type == 'decisiontree' && (this.tree.content[i].id == this.treeId || this.tree.content[i].id == this.treeId+'_')) {
 			var decisions = this.tree.content[i].decision;
-			for ( c = 0; c < decisions.length; c++) {
-				if (decisions[c].code == code || code == '*') {
-					return {
-						alternatives : decisions[c].alternative,
-						meta : this.tree.content[i].metadata
-					};
+			var meta = this.tree.content[i].metadata;
+			// Finding  decision
+			if (_id != undefined) {
+				for ( c = 0; c < decisions.length; c++) {
+					if (decisions[c].id == _id) {
+						return {
+							alternatives : decisions[c].alternative,
+							meta : meta
+						};
+					}
 				}
+			} else {
+				return {
+					alternatives : decisions[0].alternative,
+					meta : meta
+				};
 			}
 		}
 	}
 	return null;
+
 }
 module.exports = Dichotom;
--- Resources/module/dichotom.window.js
+++ Resources/module/dichotom.window.js
@@ -1,21 +1,105 @@
-exports.create = function() {
-	var self = require('module/win').create('Holzige Pflanzen');
-	var Dichotom = require('module/dichotom.model');
-	var WoodenPlants = new Dichotom('holzigePflanzen');
-	var meta = WoodenPlants.getMeta();
-	console.log(meta);
-	var decision = WoodenPlants.getDecisiontree().decision;
-	console.log(decision)
-	var tv = Ti.UI.createTableView();
-	self.add(tv)
-	var rows = [];
-	for (var i = 0; i < decision.length; i++) {
-		rows[i] = Ti.UI.createTableViewRow({
-			hasChild : true,
-			title : decision[i].statement
-		});
+function html2utf8(foo) {
+	return foo.replace(/&nbsp;/g, ' ').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+}
 
+exports.create = function(_id) {
+	var self = require('module/win').create('Holzige Pflanzen');
+	self.setLayout('vertical');
+	if (_id) {
+		var leftButton = Ti.UI.createButton({
+			title : 'Zurück'
+		});
+		self.leftNavButton = leftButton;
+		leftButton.addEventListener('click', function() {
+			self.close()
+		});
 	}
-	tv.setData(rows);
-	return self
+	setTimeout(function() {
+		if (!Ti.App.Dichotom) {
+			var dichotom = require('module/dichotom.model');
+			Ti.App.Dichotom = new dichotom('holzigePflanzen');
+		}
+		var decision = Ti.App.Dichotom.getDecisionById(_id);
+
+		if (!decision)
+			return self;
+		var alternatives = decision.alternatives;
+		var head = Ti.UI.createView({
+			top : -80,
+			backgroundColor : '#383',
+			height : Ti.UI.SIZE,
+			bottom : 0,
+			layout : 'horizontal'
+		});
+		if (decision.meta.icon_url)
+			head.add(Ti.UI.createImageView({
+				image : decision.meta.icon_url,
+				top : 0,
+				left : 5,
+				color : 'white',
+				bottom : 0,
+				width : 80,
+				height : 80,
+				height : Ti.UI.SIZE
+			}));
+		head.add(Ti.UI.createLabel({
+			text : html2utf8(decision.meta.title),
+			top : 10,
+			left : 10,
+			right : 10,
+			color : 'white',
+			bottom : 10,
+			font : {
+				fontSize : 14
+			},
+			height : Ti.UI.SIZE
+		}));
+		self.add(head);
+		var tv = Ti.UI.createTableView();
+		self.add(tv)
+		var rows = [];
+		for (var i = 0; i < alternatives.length; i++) {
+			var alt = alternatives[i];
+			rows[i] = Ti.UI.createTableViewRow({
+				hasChild : true,
+				layout : 'horizontal',
+				next_id : alt.result.next_id,
+				height : Ti.UI.SIZE
+			});
+			if (alt.media[0] && alt.media[0].url_420px) {
+				var img = Ti.UI.createImageView({
+					image : alt.media[0].url_420px,
+					top : 10,
+					left : 0,
+					width : 80,
+					height : 80,
+					bubbleParent : false
+				});
+				rows[i].add(img);
+			}
+			rows[i].add(Ti.UI.createLabel({
+				width : Ti.UI.FILL,
+				left : (alt.media[0] && alt.media[0].url_420px) ? 10 : 10,
+				top : 10,
+				right : 10,
+				height : Ti.UI.SIZE,
+				bottom : 10,
+				color : '#444',
+				text : html2utf8(alternatives[i].statement),
+				font : {
+					fontSize : Ti.UI.CONF.fontsize_title,
+					fontWeight : 'bold',
+					fontFamily : 'TheSans-B7Bold'
+				},
+			}));
+		}
+		tv.setData(rows);
+		tv.addEventListener('click', function(_e) {
+			self.tab.open(require('module/dichotom.window').create(_e.rowData.next_id));
+		});
+		self.addEventListener('close', function() {
+			self = null;
+		})
+	}, 10);
+	return self;
 }
